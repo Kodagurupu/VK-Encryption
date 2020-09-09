9ae216920228a8764acb725139cd9536816bd748
deCodedLife <dcodedlife@gmail.com>
2020-09-09 09:36:10 +0300

    Switch to android studio

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vk_encode/main.dart';
+import 'package:VK/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
